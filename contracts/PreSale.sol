9f674be525d17e0e10afdce4958a63b654702ba1
tomtsaoao <tom.tsao0@gmail.com>
2023-06-24 11:33:51 +0800

    feat: foundry lib

--- contracts/PreSale.sol
+++ contracts/PreSale.sol
@@ -8,38 +8,41 @@ import "@openzeppelin/contracts/access/Ownable2Step.sol";
 import "./IPreSale.sol";
 
 contract PreSale is IPreSale, Ownable2Step, ReentrancyGuard {
+    mapping(address => uint256) private _balances;
 
-    mapping(address => uint256) balances;
-
-    function raisedAmount() public view returns (uint256) {
+    function totalRaised() public view returns (uint256) {
         return address(this).balance;
+    }
+
+    function balanceOf(address _account) external view returns (uint256) {
+        return _balances[_account];
     }
 
     receive() external payable {
         uint256 amount = msg.value;
 
-        balances[msg.sender] += amount;
+        _balances[msg.sender] += amount;
 
         emit Deposit(amount, msg.sender);
     }
 
     function withdraw(address payable _to) external onlyOwner {
-        uint256 amount = raisedAmount();
+        uint256 amount = totalRaised();
 
-        (bool success, bytes memory data) = _to.call{value: amount}("");
+        (bool success,) = _to.call{value: amount}("");
         require(success, "FAILED_WITHDRAW");
 
         emit Withdrawal(amount, _to, msg.sender);
     }
 
     function refund(address payable _to) external nonReentrant {
-        require(balances[msg.sender] > 0, "ZERO_BALANCE");
+        require(_balances[msg.sender] > 0, "ZERO_BALANCE");
 
-        uint256 amount = balances[msg.sender];
+        uint256 amount = _balances[msg.sender];
 
-        balances[msg.sender] = 0;
+        _balances[msg.sender] = 0;
 
-        (bool success, bytes memory data) = _to.call{value: amount}("");
+        (bool success,) = _to.call{value: amount}("");
         require(success, "FAILED_REFUND");
 
         emit Refund(amount, msg.sender);
