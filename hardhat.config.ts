cc2106eba321798526504007fe31db9bd4e04640
tomtsaoao <tom.tsao0@gmail.com>
2023-06-24 11:48:00 +0800

    feat: network config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,29 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
+import * as process from "process";
+
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    arbitrumGoerli: {
+      url: "https://goerli-rollup.arbitrum.io/rpc",
+      chainId: 421613,
+      accounts: [process.env.GOERLI_TESTNET_PRIVATE_KEY!],
+    },
+    arbitrumOne: {
+      url: "https://arb1.arbitrum.io/rpc",
+      accounts: [process.env.ARBITRUM_MAINNET_PRIVATE_KEY!],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
