5ecfe547f7f404fc3ab5a7b9bdaaf9c423c0f73d
Kevin Upton <k_upton@live.com>
2023-08-26 00:27:01 +1000

    updated deployment

--- config.ts
+++ config.ts
@@ -1,48 +1,38 @@
 import { IPresale } from "./typechain-types";
 import { BigNumber } from "ethers";
 
-enum RoundType {
-  Liquidity,
-  Tokens,
-}
-
 export const rounds: IPresale.RoundConfigStruct[] = [
   {
     tokenPrice: makePrice(0.07),
     tokenAllocation: makeTokens(2e6),
-    roundType: RoundType.Tokens,
   },
   {
     tokenPrice: makePrice(0.075),
     tokenAllocation: makeTokens(2e6),
-    roundType: RoundType.Tokens,
   },
   {
     tokenPrice: makePrice(0.08),
     tokenAllocation: makeTokens(3e6),
-    roundType: RoundType.Tokens,
   },
   {
     tokenPrice: makePrice(0.085),
     tokenAllocation: makeTokens(6e6),
-    roundType: RoundType.Tokens,
   },
   {
     tokenPrice: makePrice(0.09),
     tokenAllocation: makeTokens(8e6),
-    roundType: RoundType.Tokens,
   },
   {
     tokenPrice: makePrice(0.095),
     tokenAllocation: makeTokens(8e6),
-    roundType: RoundType.Tokens,
   },
   {
     tokenPrice: makePrice(0.1),
     tokenAllocation: makeTokens(12e6),
-    roundType: RoundType.Liquidity,
   },
 ];
+
+export const totalTokenAllocation = rounds.reduce((total, current) => (total.add(current.tokenAllocation)), BigNumber.from(0));
 
 function makePrice(price: number) {
   return BigNumber.from(Math.round(price * 1e3)).mul(
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,27 +1,35 @@
-import {Presale__factory, PresaleRouter__factory} from "../typechain-types";
+import {PlaceholderToken__factory, Presale, Presale__factory, PresaleRouter__factory} from "../typechain-types";
 import { BigNumber, Signer } from "ethers";
 import hre, { ethers } from "hardhat";
-import { rounds } from "../config";
+import {rounds, totalTokenAllocation} from "../config";
 
 async function main() {
   const [signer] = await ethers.getSigners();
   const presaleFactory = new Presale__factory(signer as unknown as Signer);
   const presaleRouterFactory = new PresaleRouter__factory(signer as unknown as Signer);
+  const placeholderFactory = new PlaceholderToken__factory(signer as unknown as Signer);
 
   const swapRouter = '0xE592427A0AEce92De3Edee1F18E0157C05861564';
-  const args: Parameters<Presale__factory["deploy"]> = [
-    "0x2039f3B58ed4a8EDe1B6C8f43aC4c3DEe3187f7b",
+  const presaleAsset = '0x2039f3B58ed4a8EDe1B6C8f43aC4c3DEe3187f7b';
+  const withdrawTo = '0x921d360aD22A6D0289ce57fcb8250e299cB19EA3';
+  const initializeArgs: Parameters<Presale["initialize"]> = [
+    withdrawTo,
     {
       minDepositAmount: 0,
       maxUserAllocation: BigNumber.from(10).pow(14),
       startDate: Math.round(Date.now() / 1000) + 60,
-      withdrawTo: "0x921d360aD22A6D0289ce57fcb8250e299cB19EA3",
     },
     rounds,
   ];
 
+  const presaleToken = await placeholderFactory.deploy(signer.address, totalTokenAllocation);
+
+  console.log('PresaleToken @ ', presaleToken.address);
+
+  await presaleToken.deployed();
+
   // goerli configuration
-  const presale = await presaleFactory.deploy(...args);
+  const presale = await presaleFactory.deploy(presaleAsset, presaleToken.address);
 
   console.log(`Presale @ ${presale.address}`);
 
@@ -33,13 +41,21 @@ async function main() {
 
   await presaleRouter.deployed();
 
+  await presaleToken.approve(presale.address, totalTokenAllocation).then(tx => tx.wait());
+  await presale.initialize(...initializeArgs).then(tx => tx.wait());
+
   await new Promise((res) => setTimeout(res, 30000));
 
   console.log("verifying");
 
   await hre.run("verify:verify", {
+    address: presaleToken.address,
+    constructorArguments: [signer.address, totalTokenAllocation.toString()],
+  });
+
+  await hre.run("verify:verify", {
     address: presale.address,
-    constructorArguments: args,
+    constructorArguments: [presaleAsset, presaleToken.address],
   });
 
   await hre.run("verify:verify", {
