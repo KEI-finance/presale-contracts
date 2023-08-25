463d75e3608aac1dc4697f4c7ef526ac3ef5489a
Kevin Upton <k_upton@live.com>
2023-08-26 00:15:26 +1000

    updated swap router

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,4 +1,4 @@
-import { Presale__factory } from "../typechain-types";
+import {Presale__factory, PresaleRouter__factory} from "../typechain-types";
 import { BigNumber, Signer } from "ethers";
 import hre, { ethers } from "hardhat";
 import { rounds } from "../config";
@@ -6,7 +6,9 @@ import { rounds } from "../config";
 async function main() {
   const [signer] = await ethers.getSigners();
   const presaleFactory = new Presale__factory(signer as unknown as Signer);
+  const presaleRouterFactory = new PresaleRouter__factory(signer as unknown as Signer);
 
+  const swapRouter = '0xE592427A0AEce92De3Edee1F18E0157C05861564';
   const args: Parameters<Presale__factory["deploy"]> = [
     "0x2039f3B58ed4a8EDe1B6C8f43aC4c3DEe3187f7b",
     {
@@ -21,9 +23,15 @@ async function main() {
   // goerli configuration
   const presale = await presaleFactory.deploy(...args);
 
-  console.log(`Deployed to ${presale.address}`);
+  console.log(`Presale @ ${presale.address}`);
 
   await presale.deployed();
+
+  const presaleRouter = await presaleRouterFactory.deploy(presale.address, swapRouter);
+
+  console.log(`PresaleRouter @ ${presaleRouter.address}`);
+
+  await presaleRouter.deployed();
 
   await new Promise((res) => setTimeout(res, 30000));
 
@@ -32,6 +40,11 @@ async function main() {
   await hre.run("verify:verify", {
     address: presale.address,
     constructorArguments: args,
+  });
+
+  await hre.run("verify:verify", {
+    address: presaleRouter.address,
+    constructorArguments: [presale.address, swapRouter],
   });
 
   console.log("completed");
