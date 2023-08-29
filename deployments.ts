3e03d7d7e17f7121b39043beb7c4464e3932f305
Kevin Upton <k_upton@live.com>
2023-08-29 23:42:58 +1000

    updated deployments

--- deployments.ts
+++ deployments.ts
@@ -1,6 +1,11 @@
 export const deployments = {
+  arbitrumOne: {
+    placeholderToken: "0x0eD12993E044cdceAd432fb6822DFfFCbfA5f60d",
+    presaleRouter: "0xA0D4c5782ca11d1371F11fa77AFA10D10aF3b2f5",
+    presale: "0x67d86273c380aD84fed484646d0B67a64fBf947c",
+  },
   goerli: {
-    placeholderToken: "0xE17ac3633Ca6158e28De6e76B9CE824aeE9c242a",
+    placeholderToken: "0xeE3f5dd790703c22F8f70bD04499e08DAE28b6c1",
     presaleRouter: "0x731fd6185Def573cf4B540ae29e8c7584aE55A68",
     presale: "0x3C367B3279c362e0eC57EEb90f7899D6cc1b4D67",
   },
