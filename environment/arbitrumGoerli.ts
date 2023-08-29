03ce7445b0ebd88c1c5b6c1fb3c335adb9d3c082
Kevin Upton <k_upton@live.com>
2023-08-29 12:35:51 +1000

    updated deployment process

--- environment/arbitrumGoerli.ts
+++ environment/arbitrumGoerli.ts
@@ -2,7 +2,7 @@ export const environment = {
   stargate: {
     swapRouter: "0xBFB197dAB8ADC9560F32198CFA5454beBE2655A0", // SwapRouter
     stargateRouter: "0xb850873f4c993Ac2405A1AdD71F6ca5D4d4d6b4f",
-    presaleRouter: "0xc1e63e930B5a725c25C708658ccba6b0CEd641F8",
+    presaleRouter: "0x731fd6185Def573cf4B540ae29e8c7584aE55A68",
     presaleAsset: "0x533046F316590C19d99c74eE661c6d541b64471C",
     stargatePoolId: 2,
     presaleChainId: 10121,
--- environment/polygonMumbai.ts
+++ environment/polygonMumbai.ts
@@ -2,7 +2,7 @@ export const environment = {
   stargate: {
     swapRouter: "0xE592427A0AEce92De3Edee1F18E0157C05861564", // SwapRouter
     stargateRouter: "0x817436a076060D158204d955E5403b6Ed0A5fac0",
-    presaleRouter: "0xc1e63e930B5a725c25C708658ccba6b0CEd641F8",
+    presaleRouter: "0x731fd6185Def573cf4B540ae29e8c7584aE55A68",
     presaleAsset: "0x6Fc340be8e378c2fF56476409eF48dA9a3B781a0",
     stargatePoolId: 2,
     presaleChainId: 10121,
